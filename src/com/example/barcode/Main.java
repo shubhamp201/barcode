272361c29340e1d08fcad28dd5440f27edba7306
Luke <sharlock@gmail.com>
2014-08-27 21:16:05 +1000

    finish

--- src/com/example/barcode/Main.java
+++ src/com/example/barcode/Main.java
@@ -1,10 +1,21 @@
 package com.example.barcode;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class Main extends Activity
+public class Main extends Activity implements View.OnClickListener
 {
+
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
+
     /**
      * Called when the activity is first created.
      */
@@ -13,5 +24,38 @@ public class Main extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        scanBtn = (Button) findViewById(R.id.scan_button);
+        formatTxt = (TextView) findViewById(R.id.scan_format);
+        contentTxt = (TextView) findViewById(R.id.scan_content);
+        scanBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.scan_button)
+        {
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null)
+        {
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("FORMAT: " + scanFormat);
+            contentTxt.setText("CONTENT: " + scanContent);
+        }
+        else
+        {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
