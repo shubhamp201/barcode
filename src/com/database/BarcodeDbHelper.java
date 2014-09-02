69fd12db1746f0b12994e84d4a1887c7469a9134
Luke <sharlock@gmail.com>
2014-09-02 23:04:14 +1000

    improve the db and add list view

--- src/com/database/BarcodeDbHelper.java
+++ src/com/database/BarcodeDbHelper.java
@@ -3,6 +3,7 @@ package com.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.database.BarcodeContract.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,20 +16,31 @@ public class BarcodeDbHelper extends SQLiteOpenHelper
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "Barcode.db";
 
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + Barcode.TABLE_NAME + " (" +
+                    Barcode._ID + " INTEGER PRIMARY KEY," +
+                    Barcode.COLUMN_NAME_BARCODE_NAME + TEXT_TYPE + COMMA_SEP +
+                    Barcode.COLUMN_NAME_BARCODE_CONTENT + TEXT_TYPE + COMMA_SEP +
+                    Barcode.COLUMN_NAME_BARCODE_FORMAT + TEXT_TYPE + COMMA_SEP +
+            " )";
+
     public BarcodeDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    public void onCreate(SQLiteDatabase db)
     {
-
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2)
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
     {
-
+        onUpgrade(db, oldVersion, newVersion);
     }
 }
--- src/com/example/barcode/Main.java
+++ src/com/example/barcode/Main.java
@@ -1,14 +1,17 @@
 package com.example.barcode;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
+import com.database.BarcodeContract;
+import com.database.BarcodeDbHelper;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
@@ -16,12 +19,14 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.database.BarcodeContract.*;
 
 import java.util.EnumMap;
 import java.util.Map;
 
 public class Main extends Activity
 {
+    private SQLiteOpenHelper dbHelper;
 
     /**
      * Called when the activity is first created.
@@ -31,6 +36,32 @@ public class Main extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        ListView listView = (ListView)findViewById(R.id.listView);
+        dbHelper = new BarcodeDbHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+
+        String[] projection = {
+                Barcode._ID,
+                Barcode.COLUMN_NAME_BARCODE_NAME
+        };
+
+        Cursor c = db.query(
+                Barcode.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                null,                                // The columns for the WHERE clause
+                null,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                null                                 // The sort order
+        );
+        c.moveToFirst();
+        while(!c.isAfterLast())
+        {
+            String barcodeName = c.getString(c.getColumnIndex(Barcode.COLUMN_NAME_BARCODE_NAME));
+
+        }
+
     }
 
     public void scanBarcode(View view)
@@ -50,6 +81,15 @@ public class Main extends Activity
             TextView contentTxt = (TextView) findViewById(R.id.scan_content);
             formatTxt.setText("FORMAT: " + scanFormat);
             contentTxt.setText("CONTENT: " + scanContent);
+
+            ContentValues values = new ContentValues();
+            values.put(Barcode.COLUMN_NAME_BARCODE_CONTENT, scanContent);
+            values.put(Barcode.COLUMN_NAME_BARCODE_FORMAT, scanFormat);
+            values.put(Barcode.COLUMN_NAME_BARCODE_NAME, "FlyBuyer");
+
+            SQLiteDatabase db = dbHelper.getWritableDatabase();
+            db.insert(Barcode.TABLE_NAME,  null, values);
+            db.close();
 
             // barcode image
             ImageView iv = (ImageView) findViewById(R.id.imageView);
