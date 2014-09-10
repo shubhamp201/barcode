72dd8d3da585289ae87dd8315796c493be3aa5d0
Luke <sharlock@gmail.com>
2014-09-10 22:06:31 +1000

    cleanup

--- src/com/Domain/RootScreenFragment.java
+++ src/com/Domain/RootScreenFragment.java
@@ -1,6 +1,7 @@
 package com.Domain;
 
-import android.app.Fragment;
+
+import android.support.v4.app.Fragment;
 
 /**
  * Created by Luke on 10/09/2014.
--- src/com/example/barcode/RootScreenPagerAdapter.java
+++ src/com/example/barcode/RootScreenPagerAdapter.java
@@ -1,8 +1,11 @@
 package com.example.barcode;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+
+import com.Domain.RootScreenFragment;
 
 /**
  * Created by Luke on 10/09/2014.
@@ -15,7 +18,12 @@ public class RootScreenPagerAdapter extends FragmentPagerAdapter
 
     @Override
     public Fragment getItem(int i) {
-        return null;
+        Fragment fragment = new RootScreenFragment();
+        Bundle args = new Bundle();
+        // Our object is just an integer :-P
+        args.putInt(RootScreenFragment.ARG_OBJECT, i + 1);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
