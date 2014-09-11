b1a9a94fb044858b9a9d34507e4e1babebf8bc8f
Luke <sharlock@gmail.com>
2014-09-11 22:45:23 +1000

    swipe

--- src/com/Domain/RootScreenFragment.java
+++ src/com/Domain/RootScreenFragment.java
@@ -1,10 +1,31 @@
 package com.Domain;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.barcode.R;
 
 /**
  * Created by Luke on 10/09/2014.
  */
 public class RootScreenFragment extends Fragment {
+    public static final String ARG_OBJECT = "object";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        // The last two arguments ensure LayoutParams are inflated
+        // properly.
+        View rootView = inflater.inflate(
+                R.layout.fragment_collection_object, container, false);
+        Bundle args = getArguments();
+        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+                Integer.toString(args.getInt(ARG_OBJECT)));
+        return rootView;
+    }
 }
